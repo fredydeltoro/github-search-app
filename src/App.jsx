f9ea1776a1d6460815f30781a410696f1f645821
Freddy Monroy <fredydeltoro@gmail.com>
2024-06-28 22:07:03 -0600

    General layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container-fluid p-0">
+      <header className="bg-blue">
+        <nav className="navbar navbar-expand-lg navbar-dark">
+          <div className="container">
+            <Link className="navbar-brand" to="/">
+              <h1 className="h4 m-0">GitHub Search</h1>
+            </Link>
+            <div className="d-flex flex-grow-1 justify-content-center">
+              <div className="search-container">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="form-control"
+                  aria-label="Search"
+                />
+              </div>
+            </div>
+            <div>
+              <Link to="/users" className="users-btn btn btn-outline-light">
+                USERS
+              </Link>
+              <Link to="/repos" className="repos-btn btn btn-outline-light">
+                REPOS
+              </Link>
+            </div>
+          </div>
+        </nav>
+      </header>
+
+      <main className="py-4">
+        <div className="container">
+          <Routes>
+            <Route path="/users" element={<div>Users Search Page</div>} />
+            <Route
+              path="/repos"
+              element={<div>Repositories Search Page</div>}
+            />
+          </Routes>
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
