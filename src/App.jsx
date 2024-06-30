5c52fbf089a2974f3fc4e7711e370bdbe5628e10
Freddy Monroy <fredydeltoro@gmail.com>
2024-06-29 18:35:04 -0600

    Navitaion design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes, Link, NavLink } from "react-router-dom";
 import "./App.css";
 
 function App() {
@@ -22,12 +22,30 @@ function App() {
               </div>
             </div>
             <div>
-              <Link to="/users" className="users-btn btn btn-outline-light">
-                USERS
-              </Link>
-              <Link to="/repos" className="repos-btn btn btn-outline-light">
-                REPOS
-              </Link>
+              <NavLink
+                to="/users"
+                className={({ isActive }) =>
+                  `btn users-btn btn btn-outline-light ${
+                    isActive ? "active" : ""
+                  }`
+                }
+              >
+                <i class="active-icon bi bi-check-lg"></i>
+                <i class="status-icon bi bi-people-fill"></i>
+                <span>Users</span>
+              </NavLink>
+              <NavLink
+                to="/repos"
+                className={({ isActive }) =>
+                  `btn repos-btn btn btn-outline-light ${
+                    isActive ? "active" : ""
+                  }`
+                }
+              >
+                <i class="active-icon bi bi-check-lg"></i>
+                <i class="status-icon bi bi-journal-bookmark-fill"></i>
+                <span>Repos</span>
+              </NavLink>
             </div>
           </div>
         </nav>
