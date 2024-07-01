e76b840dcec70b402a600b600429659ce0b6cd1e
Freddy Monroy <fredydeltoro@gmail.com>
2024-06-30 23:54:22 -0600

    RepoCard documentation

--- src/components/RepoCard.jsx
+++ src/components/RepoCard.jsx
@@ -2,6 +2,25 @@ import React from "react";
 import convertToKM from "../utils/convertToKM";
 import { formatDistanceToNow } from "date-fns";
 
+/**
+ * RepoCard Component
+ *
+ * This component renders a card displaying details of a GitHub repository.
+ * It shows the repository owner's avatar, name, and repository information including
+ * language, stars, last update time, and description.
+ *
+ * @param {Object} props - The properties object.
+ * @param {Object} props.repo - The repository object.
+ * @param {Object} props.repo.owner - The owner of the repository.
+ * @param {string} props.repo.name - The name of the repository.
+ * @param {string} props.repo.language - The primary language of the repository.
+ * @param {number} props.repo.stargazers_count - The number of stars the repository has.
+ * @param {string} props.repo.updated_at - The last update timestamp of the repository.
+ * @param {string} props.repo.description - The description of the repository.
+ *
+ * @returns {JSX.Element} A card component displaying repository details.
+ */
+
 const RepoCard = ({ repo }) => {
   const isOrganization = repo.owner.type === "Organization";
 
