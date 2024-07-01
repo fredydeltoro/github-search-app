3977c56a9f69a23a0b1b64802fab46e4389514eb
Freddy Monroy <fredydeltoro@gmail.com>
2024-07-01 00:04:51 -0600

    Pagination to Repos

--- src/pages/RepoSearch.jsx
+++ src/pages/RepoSearch.jsx
@@ -1,28 +1,40 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { SearchContext } from "../SearchContext";
 import useSearch from "../hooks/useSearch";
 import RepoCard from "../components/RepoCard";
+import Paginator from "../components/Paginator";
 import convertToKM from "../utils/convertToKM";
 
 const RepoSearch = () => {
   const { searchQuery } = useContext(SearchContext);
+  const [page, setPage] = useState(1);
+  const perPage = 2;
   const { results, totalCount, loading, error } = useSearch(
     "repositories",
-    searchQuery
+    searchQuery,
+    page,
+    perPage
   );
-
-  console.log(results, totalCount);
+  const totalPages = Math.ceil(totalCount / perPage);
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  };
 
   return (
     <>
       <b>{convertToKM(totalCount)} results</b>
       <ul>
         {results.map((item) => (
-          <li className="repo-card">
+          <li className="repo-card" key={item.id}>
             <RepoCard repo={item} />
           </li>
         ))}
       </ul>
+      <Paginator
+        currentPage={page}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
     </>
   );
 };
